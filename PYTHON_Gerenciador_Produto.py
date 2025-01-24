2577a2ffb6db7bd2d0527ff24a583338e48e89ec
Iury Ferreira <1uryf3rr31r4@gmail.com>
2025-01-24 12:29:00 -0300

    Criação da Função Menu

---
+++ PYTHON_Gerenciador_Produto.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+produtos=[]
+
+def menu():
+    print('Gerenciado de Produtos'.center(100,' '))
+    print('1 - Cadastrar Produto')
+    print('2 - Remover produto')
+    print('3 - Procurar Produto')
+    print('4 - Ver Produtos')
+    print('5 - Sair')
+
+menu()
+

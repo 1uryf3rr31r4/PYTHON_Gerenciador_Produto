147af42cb24ae74388d2d4599842f206080a9731
Iury Ferreira <1uryf3rr31r4@gmail.com>
2025-01-24 13:06:15 -0300

    Função Cadastar Produto

--- PYTHON_Gerenciador_Produto.py
+++ PYTHON_Gerenciador_Produto.py
@@ -10,15 +10,25 @@ def menu():
     print('4 - Ver Produtos')
     print('5 - Sair')
 
-def adicionar_produto():
+def cadastrar_produto():
     print('Quantos produtos deseja adicionar? ')
     num=int(input('>> '))
+    for i in range(1,num+1):
+        data=datetime.now().strftime('%d/%m/%Y')
+        nome=input('Nome: ')
+        estoque=int(input('Quantidade em estoque: '))
+        preco=float(input('Preço: '))
+        descricao=input('Descrição do Produto: ')
+        produtos.append([nome, estoque, preco, data, descricao])
 
 def main():
     escolha=''
     while escolha!=5:
         menu()
         escolha=input('>> ')
+        if escolha in list('1234'):
+            if escolha=='1':
+                cadastrar_produto()
 
 if __name__== '__main__':
     main()
